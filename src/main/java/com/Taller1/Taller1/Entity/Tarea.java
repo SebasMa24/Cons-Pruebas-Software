255c8d5ed67e9c4d96f564497e32bb3dfd5ec43b
MiguelVeloza <mangel.veloza@gmail.com>
2025-09-25 16:15:55 -0500

    Implementa HU-2: agregar fecha fincalizado

--- src/main/java/com/Taller1/Taller1/Entity/Tarea.java
+++ src/main/java/com/Taller1/Taller1/Entity/Tarea.java
@@ -2,7 +2,11 @@ package com.Taller1.Taller1.Entity;
 
 import java.time.LocalDate;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -31,6 +35,9 @@ public class Tarea{
     @Column(nullable = false)
     private String estado;
 
+    @Column
+    private LocalDate fechaFinalizacion;
+
     @Override
     public String toString() {
         return "Tarea{" +
@@ -39,6 +46,7 @@ public class Tarea{
                 ", descripcion='" + descripcion + '\'' +
                 ", fechaVencimiento=" + fechaVencimiento +
                 ", estado=" + estado +
+                ", fechaFinalizacion=" + fechaFinalizacion +
                 '}';
     }
 }
